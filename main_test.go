a639d618f1e5fd5943ad567684f319789d47eae2
Alexandre Hannagan <hannagan.alexandre@gmail.com>
2023-07-25 15:49:42 +0200

    Fixed unit test

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ import (
 func TestFileCreation(t *testing.T) {
 
 	// Date au format D_M_YYYY_Hh_Mm
-	currentDate := strconv.Itoa(time.Now().Day()) + "_" + strconv.Itoa(int(time.Now().Month())) + "_" + strconv.Itoa(time.Now().Year()) + "_" + strconv.Itoa(time.Now().Hour()) + "h" + strconv.Itoa(time.Now().Minute()) + "m"
+	currentDate := strconv.Itoa(time.Now().Day()) + "_" + strconv.Itoa(int(time.Now().Month())) + "_" + strconv.Itoa(time.Now().Year()) + "_" + strconv.Itoa(time.Now().Hour()+2) + "h" + strconv.Itoa(time.Now().Minute()+3) + "m"
 
 	filename := "Archive/" + currentDate + ".txt"
 
