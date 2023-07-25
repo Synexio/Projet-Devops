678cb0d5b506b3bdf1c2abb21c9f99c053edb09d
Alexandre Hannagan <hannagan.alexandre@gmail.com>
2023-07-25 15:04:33 +0200

    Added unit test and new GA workflow

---
+++ main_test.go
@@ -0,0 +1,21 @@
+// main_test.go
+package main
+
+import (
+	"os"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestFileCreation(t *testing.T) {
+
+	// Date au format D_M_YYYY_Hh_Mm
+	currentDate := strconv.Itoa(time.Now().Day()) + "_" + strconv.Itoa(int(time.Now().Month())) + "_" + strconv.Itoa(time.Now().Year()) + "_" + strconv.Itoa(time.Now().Hour()) + "h" + strconv.Itoa(time.Now().Minute()) + "m"
+
+	filename := "Archive/" + currentDate + ".txt"
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		t.Errorf("File was not created in the Archive folder: %s", filename)
+	}
+}
